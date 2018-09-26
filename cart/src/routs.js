01501a4006d4456e2d383a2044db8f1f4baaea8c
Swati Babariya <swati.babariya@infostretch.com>
2018-09-26 18:55:40 +0530

    cart added

---
+++ cart/src/routs.js
@@ -0,0 +1,20 @@
+import cart from './cart.vue';
+import top from './top.vue';
+import checkout from './checkout.vue';
+export const routes=[
+    {
+        path:'/cart',
+        component:cart
+
+},
+{
+    path:'/top',
+    component:top
+
+},
+{
+    path:'/checkout',
+    component:checkout
+
+}
+]
